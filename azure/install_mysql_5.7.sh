9035cd78cbf94fcc728a4b23dde8438271764f60
Kwilt <ken@72lux.com>
2016-09-16 11:04:17 -0400

    mount drive before installin mysql

--- azure/install_mysql_5.7.sh
+++ azure/install_mysql_5.7.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+echo "Mounting data drive .............."
+(echo o; echo n; echo p; echo 1; echo ; echo; echo w) | sudo fdisk /dev/sdc
+sudo mkfs -t ext4 /dev/sdc1
+sudo mkdir /data
+sudo mount /dev/sdc1 /data
+#remount on boot
+sudo chmod 777 /etc/fstab
+sudo echo "/dev/sdc1       /data   ext4    defaults,nofail        0       2" >> /etc/fstab
+sudo chmod 644 /etc/fstab
+echo "Data dir now exists and will be remounted on boot"
+
 mysqlPassword=$1
 sudo apt-get -y update
 sudo apt-get -y upgrade
